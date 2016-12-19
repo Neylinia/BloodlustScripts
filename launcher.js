8396f4db4cf9763846d39e5f8d87e2adca94a694
Mélanie Chastel <chastel.melanie@me.com>
2016-12-19 14:15:31 +0100

    ajout spliter et variable globale

--- launcher.js
+++ launcher.js
@@ -1,24 +1,43 @@
+on('ready',function() {
+    "use strict";
+
+    // Check if the namespaced property exists, creating it if it doesn't
+    if( ! state.AttackNS ) {
+        state.AttackNS={
+            menace: 0
+        };
+        log("AttackNS created");
+    }else{
+        state.AtttackNS.menace=0;
+    }
+});
+
+function spliter(message){
+  var commande = split(message);
+}
+
 on("chat:message", function(msg){
   if(msg.type == "api" && msg.content.indexOf('!help ') !== -1) {
     help(msg);
   }else if (msg.type == "api") {
     var pers;
-    pers = getPersonnage(msg.who, getAllPersonnages())
+    pers = getPersonnage(msg.who, getAllPersonnages());
     if(pers){
+      var commande = spliter(msg.content);
       if(msg.content.indexOf('!roll ') !== -1){
-
+        competence(false,commande,pers,msg);
       }else if (msg.content.indexOf('!gmRoll ') !== -1) {
-
+        competence(true,commande,pers,msg);
       }else if (msg.content.indexOf('!attack ') !== -1) {
-
+        attack(false,commande,pers,msg);
       }else if (msg.content.indexOf('!gmAttack ') !== -1) {
-
+        attack(true,commande,pers,msg);
       }else if (msg.content.indexOf('!defend ') !== -1) {
-
+        defend(false,commande,pers,msg);
       }else if (msg.content.indexOf('!gmDefend ') !== -1) {
-
+        defend(true,commande,pers,msg);
       }else if (msg.content.indexOf('!encaisse ') !== -1) {
-
+        encaisse(commande,pers,msg);
       }
     }else {
       sendChat("RollBot","/w "+msg.who+", Veuillez utiliser votre personnage pour lancer cette commande");
