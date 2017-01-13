b4dcba6196077fc107755b99dd97559908ff2480
Mélanie Chastel <chastel.melanie@me.com>
2017-01-13 15:25:31 +0100

    Ajout calcul compétence

--- competence.js
+++ competence.js
@@ -1,5 +1,15 @@
 function aspectRedirect(arg, personnage){
-
+  var aspect;
+  if(arg.includes("a")){
+    aspect = getAspectPersonnage(false, arg.substring(0,1), personnage);
+  } else if(arg.includes("w")){
+    var personnages = getAllPersonnages();
+    var arme = getArme(personnage, personnages);
+    aspect = getAspectArme(arg.substring(0,1), arme);
+  } else if(arg.includes("f"){
+    aspect = getAspectPersonnage(true, arg.substring(0,1), personnage);
+  }
+  return aspect;
 }
 
 function calculComp(commande,personnage){
@@ -17,14 +27,22 @@ function calculComp(commande,personnage){
 
   var i = 2;
 
-  while (true) {
-
-    /*if (contient a f w){
-        aspectRedirect
-      }else if (contient r d){
-        risques_difficultés
-      }
-    */
+  while (i<=(commande.length - 1)) {
+    if((commande[i].includes("a"))||(commande[i].includes("w"))||(commande[i].includes("f"))){
+      result[i] = aspectRedirect(commande[i],personnage);
+      nbDes = nbDes + result[i].valeur;
+      nbDes = nbDes + result[i].nbDesSang;
+      result[0].desSang = result[i].nbDesSang;
+    } else if(commande[i].includes("r")){
+      var risques = parseInt(commande[i].substring(0,1));
+      nbDes = nbDes - risques;
+      result[0].risques = risques;
+    } else if(commande[i].includes("d")) {
+      var difficulte = parseInt(commande[i].substring(0,2));
+      nbDes = nbDes + difficulte;
+      result[0].difficulte = difficulte;
+    }
+    i++;
   }
 
   result[0].nbDes = nbDes;
