99406ab93e37a0167d8be045e0fceae7ae033cd4
Mélanie Chastel <chastel.melanie@me.com>
2017-01-23 15:24:23 +0100

    Copie depuis Roll20

--- launcher.js
+++ launcher.js
@@ -13,35 +13,36 @@ on('ready',function() {
 });
 
 function spliter(message){
-  var messageMin = message.toLowerCase();
-  var commande = split(messageMin);
+    var messageMin = message.toLowerCase();
+    var commande = messageMin.split(" ");
+    return commande;
 }
-
+      
 on("chat:message", function(msg){
-  if(msg.type == "api" && msg.content.indexOf('!help ') !== -1) {
-    help(msg);
-  }else if (msg.type == "api") {
-    var pers;
-    pers = getPersonnage(msg.who, getAllPersonnages());
-    if(pers){
-      var commande = spliter(msg.content);
-      if(msg.content.indexOf('!roll ') !== -1){
-        competence(false,commande,pers,msg);
-      }else if (msg.content.indexOf('!gmRoll ') !== -1) {
-        competence(true,commande,pers,msg);
-      }else if (msg.content.indexOf('!attack ') !== -1) {
-        attack(false,commande,pers,msg);
-      }else if (msg.content.indexOf('!gmAttack ') !== -1) {
-        attack(true,commande,pers,msg);
-      }else if (msg.content.indexOf('!defend ') !== -1) {
-        defend(false,commande,pers,msg);
-      }else if (msg.content.indexOf('!gmDefend ') !== -1) {
-        defend(true,commande,pers,msg);
-      }else if (msg.content.indexOf('!encaisse ') !== -1) {
-        encaisse(commande,pers,msg);
-      }
-    }else {
-      sendChat("RollBot","/w "+msg.who+", Veuillez utiliser votre personnage pour lancer cette commande");
-    }
-  }
-});
+    if(msg.type == "api" && msg.content.indexOf('!help ') !== -1) {
+          help(msg);
+    }else if (msg.type == "api") {
+        var pers;
+        pers = getPersonnage(msg.who, getAllPersonnages());
+        if(pers){
+            var commande = spliter(msg.content);
+            if(msg.content.indexOf('!roll ') !== -1){
+              competence(false,commande,pers,msg);
+            }else if (msg.content.indexOf('!gmRoll ') !== -1) {
+              competence(true,commande,pers,msg);
+            }else if (msg.content.indexOf('!attack ') !== -1) {
+              attack(false,commande,pers,msg);
+            }else if (msg.content.indexOf('!gmAttack ') !== -1) {
+              attack(true,commande,pers,msg);
+            }else if (msg.content.indexOf('!defend ') !== -1) {
+              defend(false,commande,pers,msg);
+            }else if (msg.content.indexOf('!gmDefend ') !== -1) {
+              defend(true,commande,pers,msg);
+            }else if (msg.content.indexOf('!encaisse ') !== -1) {
+              encaisse(commande,pers,msg);
+            }
+          }else {
+            sendChat("RollBot","/w "+msg.who+", Veuillez utiliser votre personnage pour lancer cette commande");
+          }
+        }
+      });
