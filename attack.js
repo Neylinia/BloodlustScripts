4eb4ef98409ac0492aa2ef3d548cde2726023405
Mélanie Chastel <chastel.melanie@me.com>
2017-02-22 15:58:35 +0100

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,3 +1,18 @@
+
+function calculComp(commande,personnage){
+  
+}
+
+function affichAttack(gm,resCommande,msg){
+  
+}
+
 function attack(gm,commande,personnage,msg){
+  var resCommande = calculAttack(commande,personnage);
+  if(resCommande == "STOP"){
+    sendChat("RollBot","/w "+msg.who+", Vous n'avez pas assez d'Effort pour faire ce test");
+  }else{
+    affichAttack(gm,resCommande,msg);
+  }
 
 }
